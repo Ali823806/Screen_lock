5b8c64bc569867c342e16072a62f50749299dc60
Ali823806 <alisalahk57@gmail.com>
2021-07-01 17:17:11 +0300

    v2.1 screen lock

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 var e = document.getElementById('lock')
 var p = PatternLock(e, {
     onPattern:function(){
-        this.success()
+        this.success($(document).ready(function(){
+            $(".aa"&&".bb"&&".cc").mouseup(function(){
+                alert ("You have entered the corect password");
+            });
+        }))
     }
+
 })
 
-const int ROW_NUM    = 4; //four rows
-const int COLUMN_NUM = 4; //four columns
+keys [row_num][column_num] =
+[
+    [a,'2','3'],
+    ['4',b,'6'],
+    ['7','8',c]
+];
+
+this.keys = document.getElementById('lock');
+this.a = document.getElementsByClassName ('aa');
+this.b = document.getElementsByClassName ('bb');
+this.c = document.getElementsByClassName ('cc');
+
+$(document).ready(function(){
+    $(".aa"&&".bb"&&".cc").mouseup(function(){
+        alert ("Done");
+    });
+});
 
 
 
-   keys [row_num][column_num] =
-  [
-      ['1','2','3'],
-      ['4','5','6'],
-      ['7','8','9']
-  ];
+
+  
+
 
  
 
