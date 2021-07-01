c7466183324c15c268a41c0cdfe0669ebf9123e1
Ali823806 <alisalahk57@gmail.com>
2021-07-01 17:16:35 +0300

    V2 screen lock

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ var p = PatternLock(e, {
         this.success()
     }
 })
+
+const int ROW_NUM    = 4; //four rows
+const int COLUMN_NUM = 4; //four columns
+
+
+
+   keys [row_num][column_num] =
+  [
+      ['1','2','3'],
+      ['4','5','6'],
+      ['7','8','9']
+  ];
+
+ 
+
+
+
